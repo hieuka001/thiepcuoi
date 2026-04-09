e743a10a5531f8a01a593fcc5cc6e7eb6f2ad63d
hieuka001 <127222971+hieuka001@users.noreply.github.com>
2026-04-09 20:09:37 +0700

    Add files via upload

---
+++ asset-content.js
@@ -0,0 +1,5 @@
+window.WEDDING_ASSETS = {
+  // Cac anh phu tro khong phai anh cuoi
+  giftBox: "https://pngimg.com/d/gift_PNG100271.png",
+  transferQr: "https://raw.githubusercontent.com/hieuka001/Anki-images/main/uploads/temp_upload_20260407_222734.png?size=280x280&data=MB|CHUC%20MUNG%20HANH%20PHUC"
+};
---
+++ music-content.js
@@ -0,0 +1,7 @@
+window.WEDDING_MUSIC = {
+  // Doi link nhac tai day (mp3)
+  src: "https://cdn.jsdelivr.net/gh/hieuka001/thiepcuoitinnhi@main/50nam.mp3",
+  // am luong tu 0 -> 1
+  volume: 0.75
+};
+
